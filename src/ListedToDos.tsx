1edffaa857333d79effd7ae218e83c81b0c56a82
Ben Murray <benmurray573@gmail.com>
2022-12-15 16:23:28 +0000

    recongifured to link with backend, can successfully get all todos

--- src/ListedToDos.tsx
+++ src/ListedToDos.tsx
@@ -1,36 +1,59 @@
+import axios from "axios"
 import { useEffect, useState } from "react"
 
 interface ToDo {
     id: number
-    todo: string
+    body: string
 }
 
 function ListedToDos(): JSX.Element {
-    const [toDoList, setToDoList] = useState<string[]>([])
+    const [toDoList, setToDoList] = useState<ToDo[]>([])
     const [typedToDo, setTypedToDo] = useState<string>("")
 
+    async function fetchAndStoreToDos() {
+        const fetchedToDoData = await axios.get("http://localhost:4000/todos")
+        console.log(fetchedToDoData)
+        const dataArr: ToDo[] = fetchedToDoData.data
+        setToDoList(dataArr.map((toDoData) => {
+            return toDoData
+        }));
+    }
+
     useEffect(() => {
-        fetch("https://6389f8bfc5356b25a20e0c49.mockapi.io/todos")
-            .then(response => response.json())
-            .then((fetchedToDos: ToDo[]) => setToDoList([fetchedToDos[0].todo]));
+        fetchAndStoreToDos()
     }, [])
 
-    function handleToDoSubmission() {
+    async function handleToDoSubmission() {
         if (typedToDo !== "") {
-            return setToDoList([...toDoList, typedToDo])
+            // return setToDoList([...toDoList, typedToDo])
+            const bodyString = JSON.stringify({ todo: typedToDo })
+            await fetch("http://localhost:4000/todos", {
+                method: "POST",
+                headers: { 'Content-Type': 'application/json' },
+                body: bodyString
+            })
+            fetchAndStoreToDos()
+            // .then clear input field and tell suer it has been a success;
         }
     }
 
     return (
         <>
             <input type="text" placeholder="Enter To-Do"
-                onChange={(e) => { setTypedToDo(e.target.value)}}></input>
+                onChange={(e) => { setTypedToDo(e.target.value) }}></input>
             <button onClick={handleToDoSubmission}>Submit To-Do</button>
             <ul className="todoList">
-                {toDoList.map(todo => {
+                {toDoList.map((todo)  => {
                     if (toDoList.length > 0) {
-                        return (<><input type="checkbox"></input><span>{todo}</span><hr /></>)
+                        return (
+                            <>
+                                <input type="checkbox"></input>
+                                <li key={todo.id}>{todo.body}</li>
+                                <hr />
+                            </>
+                        )
                     }
+                    else return <span>There are currently no To-Do's!</span>
                 })}
             </ul>
         </>
