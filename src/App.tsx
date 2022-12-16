8839162424e4fd4f22a7665ad429bf9f8dbaeb4d
Ben Murray <benmurray573@gmail.com>
2022-12-16 15:12:17 +0000

    GET, POST, DELETE working, basic css introduced

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import ListedToDos from "./ListedToDos";
+import "./App.css";
 
 function App(): JSX.Element {
-  return(
+  return (
     <>
       <h1>TO-DO LIST</h1>
       <h3>Sort your life out</h3>
       <ListedToDos />
     </>
-  )
+  );
 }
 
 export default App;
--- src/ListedToDos.tsx
+++ src/ListedToDos.tsx
@@ -1,63 +1,79 @@
-import axios from "axios"
-import { useEffect, useState } from "react"
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 interface ToDo {
-    id: number
-    body: string
+  id: number;
+  body: string;
 }
 
 function ListedToDos(): JSX.Element {
-    const [toDoList, setToDoList] = useState<ToDo[]>([])
-    const [typedToDo, setTypedToDo] = useState<string>("")
+  const [toDoList, setToDoList] = useState<ToDo[]>([]);
+  const [typedToDo, setTypedToDo] = useState<string>("");
 
-    async function fetchAndStoreToDos() {
-        const fetchedToDoData = await axios.get("http://localhost:4000/todos")
-        console.log(fetchedToDoData)
-        const dataArr: ToDo[] = fetchedToDoData.data
-        setToDoList(dataArr.map((toDoData) => {
-            return toDoData
-        }));
+  async function fetchAndStoreToDos() {
+    const fetchedToDoData = await axios.get("http://localhost:4000/todos");
+    console.log(fetchedToDoData);
+    const dataArr: ToDo[] = fetchedToDoData.data;
+    setToDoList(
+      dataArr.map((toDoData) => {
+        return toDoData;
+      })
+    );
+  }
+
+  useEffect(() => {
+    fetchAndStoreToDos();
+  }, []);
+
+  async function handleToDoSubmission() {
+    if (typedToDo !== "") {
+      console.log("trying to post: ", typedToDo);
+      const newToDoEntry: ToDo = { id: toDoList.length + 1, body: typedToDo };
+      await axios.post("http://localhost:4000/todos", newToDoEntry);
+      fetchAndStoreToDos();
     }
+  }
 
-    useEffect(() => {
-        fetchAndStoreToDos()
-    }, [])
+  async function handleDeleteRequest(id: number) {
+    console.log("deleting todo of id ", id);
+    await axios.delete(`http://localhost:4000/todos/${id}`);
+    fetchAndStoreToDos();
+  }
 
-    async function handleToDoSubmission() {
-        if (typedToDo !== "") {
-            // return setToDoList([...toDoList, typedToDo])
-            const bodyString = JSON.stringify({ todo: typedToDo })
-            await fetch("http://localhost:4000/todos", {
-                method: "POST",
-                headers: { 'Content-Type': 'application/json' },
-                body: bodyString
-            })
-            fetchAndStoreToDos()
-            // .then clear input field and tell suer it has been a success;
-        }
-    }
-
-    return (
-        <>
-            <input type="text" placeholder="Enter To-Do"
-                onChange={(e) => { setTypedToDo(e.target.value) }}></input>
-            <button onClick={handleToDoSubmission}>Submit To-Do</button>
-            <ul className="todoList">
-                {toDoList.map((todo)  => {
-                    if (toDoList.length > 0) {
-                        return (
-                            <>
-                                <input type="checkbox"></input>
-                                <li key={todo.id}>{todo.body}</li>
-                                <hr />
-                            </>
-                        )
-                    }
-                    else return <span>There are currently no To-Do's!</span>
-                })}
-            </ul>
-        </>
-    )
+  return (
+    <>
+      <input
+        type="text"
+        placeholder="Enter To-Do"
+        onChange={(e) => {
+          setTypedToDo(e.target.value);
+        }}
+      ></input>
+      <button onClick={handleToDoSubmission}>Submit To-Do</button>
+      <table>
+        <tbody className="todoList">
+          {toDoList.map((todo) => {
+            return (
+              <tr key={todo.id}>
+                <td className="todoBod">{todo.body}</td>
+                <td>
+                  <input type="checkbox" className="todoBox"></input>
+                </td>
+                <td>
+                  <button
+                    onClick={() => handleDeleteRequest(todo.id)}
+                    className="delbtn"
+                  >
+                    Delete To-Do
+                  </button>
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </>
+  );
 }
 
-export default ListedToDos
+export default ListedToDos;
